3bd92e164941532329e92207f38f0ffe8dd431ec
tuanhungtran678 <tuanhungtran678@gmail.com>
2026-06-11 20:58:51 +0700

    Update 2

--- js/filesystem.js
+++ js/filesystem.js
@@ -1,165 +1,156 @@
-const FileSystem = {
+class FileSystem {
 
-    Desktop: [
+    constructor() {
 
-        {
-            name: "Paint",
-            type: "app"
-        },
-
-        {
-            name: "Calculator",
-            type: "app"
-        },
-
-        {
-            name: "Games",
-            type: "app"
-        },
-
-        {
-            name: "Pictures",
-            type: "folder"
-        },
-
-        {
-            name: "Documents",
-            type: "folder"
-        }
-    ],
-
-    Pictures: [
-
-        {
-            name: "Rainbow.png",
-            type: "file"
-        },
-
-        {
-            name: "Dinosaur.jpg",
-            type: "file"
-        }
-    ],
-
-    Documents: [
-
-        {
-            name: "Homework.txt",
-            type: "file"
-        },
-
-        {
-            name: "Story.doc",
-            type: "file"
-        }
-    ],
-
-    RecycleBin: []
-};
-
-// =========================
-// HELPERS
-// =========================
-
-function getFolder(
-    folderName
-) {
-
-    return (
-        FileSystem[
-            folderName
-        ] || []
-    );
-}
-
-function createFile(
-    folder,
-    file
-) {
-
-    if (
-        !FileSystem[folder]
-    ) {
-
-        FileSystem[folder] = [];
-    }
-
-    FileSystem[folder]
-        .push(file);
-}
-
-function deleteFile(
-    folder,
-    fileName
-) {
-
-    if (
-        !FileSystem[folder]
-    ) return;
-
-    const index =
-        FileSystem[folder]
-            .findIndex(
-                file =>
-                    file.name ===
-                    fileName
+        const saved =
+            localStorage.getItem(
+                "windowskids_fs"
             );
 
-    if (
-        index === -1
-    ) return;
+        if (saved) {
 
-    const removed =
-        FileSystem[
-            folder
-        ].splice(
-            index,
-            1
-        )[0];
+            this.data =
+                JSON.parse(saved);
 
-    FileSystem
-        .RecycleBin
-        .push(removed);
+        } else {
+
+            this.data = {
+
+                Desktop: {
+
+                    type: "folder",
+
+                    children: {
+
+                        Games: {
+                            type: "folder",
+                            children: {}
+                        },
+
+                        Pictures: {
+                            type: "folder",
+                            children: {}
+                        },
+
+                        "Homework.doc": {
+                            type: "file"
+                        },
+
+                        "Music.mp3": {
+                            type: "file"
+                        }
+                    }
+                },
+
+                RecycleBin: {
+
+                    type: "folder",
+
+                    children: {}
+                }
+            };
+
+            this.save();
+        }
+    }
+
+    save() {
+
+        localStorage.setItem(
+            "windowskids_fs",
+            JSON.stringify(
+                this.data
+            )
+        );
+    }
+
+    getFolder(path) {
+
+        let current =
+            this.data;
+
+        for (const part of path) {
+
+            current =
+                current[part];
+
+            if (
+                current &&
+                current.children
+            ) {
+
+                current =
+                    current.children;
+            }
+        }
+
+        return current;
+    }
+
+    createFolder(
+        path,
+        folderName
+    ) {
+
+        const folder =
+            this.getFolder(path);
+
+        folder[folderName] = {
+
+            type: "folder",
+
+            children: {}
+        };
+
+        this.save();
+    }
+
+    deleteItem(
+        path,
+        itemName
+    ) {
+
+        const folder =
+            this.getFolder(path);
+
+        const item =
+            folder[itemName];
+
+        if (!item) return;
+
+        this.data
+            .RecycleBin
+            .children[itemName] =
+                item;
+
+        delete folder[itemName];
+
+        this.save();
+    }
+
+    restoreItem(
+        itemName
+    ) {
+
+        const item =
+            this.data
+                .RecycleBin
+                .children[itemName];
+
+        if (!item) return;
+
+        this.data
+            .Desktop
+            .children[itemName] =
+                item;
+
+        delete this.data
+            .RecycleBin
+            .children[itemName];
+
+        this.save();
+    }
 }
-
-// =========================
-// DEBUG
-// =========================
 
 window.FileSystem =
-    FileSystem;
-
-window.getFolder =
-    getFolder;
-
-window.createFile =
-    createFile;
-
-window.deleteFile =
-    deleteFile;
-
-    function createFolder(
-    parent,
-    folderName
-) {
-
-    if (
-        !FileSystem[parent]
-    ) return;
-
-    FileSystem[parent].push({
-
-        name:
-            folderName,
-
-        type:
-            "folder"
-    });
-
-    FileSystem[
-        folderName
-    ] = [];
-}
-
-window.createFolder =
-    createFolder;
+    new FileSystem();
